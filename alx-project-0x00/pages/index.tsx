336902e3bf4c59a176a0551e6f926296ec45df5b
Pleasure Kemefa <pleasurekemefa@gmail.com>
2026-01-18 02:40:04 -0800

    Initial Commit

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import Pill from '@/components/Pill'
-
-const Home = () => {
+const Home: React.FC = () => {
   return (
-    <div>
-      <Pill />
-    </div>
+    <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
+      <h1>Airbnb Application Clone system</h1>
+      <button className=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-full">Get Started</button>
+    </main>
   )
 }
 
-export default Home
+export default Home;
